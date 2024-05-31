06e657e70f6bdf8290ca0a92cacc10962e70d8ce
evacarvallo11 <158617101+evacarvallo11@users.noreply.github.com>
2024-05-31 15:06:15 -0400

    Create form.py

---
+++ form.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+print('Content-type: text/html\n')
+
+import cgitb #
+cgitb.enable() #These 2 lines will allow error messages to appear on a web page in the browser
+
+import cgi
+
+HTML_HEADER = """
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<title>Hello</title>
+</head>
+"""
+
+HTML_FOOTER = """
+</body>
+</html>
+"""
+
+
+data = cgi.FieldStorage()
+name = 'batman'
+if ('name' in data):
+    name = data['name'].value
+bgcolor = 'DarkSeaGreen'
+if ('bgcolor' in data):
+    bgcolor = data['bgcolor'].value
+
+html= HTML_HEADER
+html+= '<body style="background-color: '
+html+= bgcolor + ';">'
+html+= '<h1>Hello ' + name + '</h1>'
+html+= '<br><a href="hello.html">Try Again</a>'
+html+= HTML_FOOTER
+print(html)
